9b70a7d1f69546b277ad9c87b5099e085b5cdc54
Siddaarth-Babu <siddaarthbabu2006.com>
2026-02-07 14:17:29 +0530

    Added the backend branch

---
+++ backend/models.py
@@ -0,0 +1,160 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, CheckConstraint, Table
+from sqlalchemy.orm import relationship
+
+course_topic_link = Table(
+    "course_topic_link",
+    Base.metadata,
+    Column("course_id", Integer, ForeignKey("course.course_id"), primary_key=True),
+    Column("topic_id", Integer, ForeignKey("topic.topic_id"), primary_key=True)
+)
+
+course_student_link = Table(
+    "course_student_link",
+    Base.metadata,
+    Column("course_id",Integer,ForeignKey("course.course_id"),primary_key=True),
+    Column("student_id",Integer,ForeignKey("student.student_id"),primary_key=True)
+)
+
+# Relationship: Instructor "Teaches" Course (Many-to-Many)
+course_instructor_link = Table(
+    "course_instructor_link",
+    Base.metadata,
+    Column("course_id", Integer, ForeignKey("course.course_id"), primary_key=True),
+    Column("instructor_id", Integer, ForeignKey("instructor.instructor_id"), primary_key=True)
+)
+
+class Course(Base):
+    __tablename__ = "course"
+
+    course_id = Column(Integer, primary_key=True, index=True)
+    course_name = Column(String(100), nullable=False)
+    duration = Column(String(50))
+    skill_level = Column(String(50))
+    course_fees = Column(Integer)
+
+    # One-to-Many Links
+    program_id = Column(Integer, ForeignKey("program.program_id"))
+    institute_id = Column(Integer, ForeignKey("university.institute_id"))
+    # One - One
+    textbook = relationship("Textbook", back_populates="course", uselist=False)
+    # One - Many
+    videos = relationship("Video",back_populates="course")
+    notes = relationship("Notes",back_populates="course")
+    # Many-to-Many Links
+    topics = relationship("Topic", secondary=course_topic_link)
+    instructors = relationship("Instructor", secondary=course_instructor_link)
+
+
+class Student(Base):
+    __tablename__ = "student"
+
+    student_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    dob = Column(Date, nullable=False) 
+    country = Column(String(50))
+    skill_level = Column(String(50))
+    email_id = Column(String(100), unique=True, index=True, nullable=False)
+    contact_number = Column(String(20))
+    specialization = Column(String(100))
+
+    # Many to Many
+    courses = relationship("Course", secondary=course_student_link,back_populates="student")
+
+class Instructor(Base):
+    __tablename__ = "instructor"
+
+    instructor_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    department = Column(String(100))
+    email_id = Column(String(100), unique=True, nullable=False)
+
+    # Many to Many
+    courses = relationship("Course", back_populates="instructor",secondary=course_instructor_link)
+
+class Video(Base):
+    __tablename__ = "video"
+
+    video_id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100), nullable=False)
+    duration = Column(Integer,nullable=False)
+    url_link = Column(String, nullable=False)
+    course_id = Column(Integer, ForeignKey("course.course_id"))
+
+class Notes(Base):
+    __tablename__ = "notes"
+
+    notes_id = Column(Integer, primary_key=True, index=True) 
+    title = Column(String(100), nullable=False)
+    url_link = Column(String, nullable=False)
+    document_type = Column(String(20))
+    course_id = Column(Integer, ForeignKey("course.course_id"))
+
+class Textbook(Base):
+    __tablename__ = "textbook"
+
+    book_id = Column(Integer, primary_key=True, index=True)
+    title_name = Column(String(150), nullable=False)
+    author = Column(String(100))
+    publisher = Column(String(100))
+
+    course_id = Column(Integer, ForeignKey("course.course_id"),unique=True)
+    # The Relationship Link
+    course = relationship("Course",back_populates="textbook")
+
+class University(Base):
+    __tablename__ = "university"
+
+    institute_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(150), nullable=False)
+    city = Column(String(50))
+    country = Column(String(50))
+
+    # One to Many
+    courses = relationship("Course",back_populates="university")
+
+
+class SystemAdmin(Base):
+    __tablename__ = "system_administrator"
+
+    admin_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100))
+    email_id = Column(String(100), unique=True)
+
+class Topic(Base):
+    __tablename__ = "topic"
+
+    topic_id = Column(Integer, primary_key=True, index=True)
+    topic_name = Column(String(100))
+
+    # Many to Many
+    courses = relationship("Course",secondary=course_topic_link,back_populates="topic")
+
+class Program(Base):
+    __tablename__ = "program"
+
+    program_id = Column(Integer, primary_key=True, index=True)
+    program_type = Column(String(50)) 
+
+    # One to Many
+    courses = relationship("Course",back_populates="university")
+
+class DataAnalyst(Base):
+    __tablename__ = "data_analyst"
+
+    analyst_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    email_id = Column(String(100), unique=True, index=True, nullable=False)
+
+class Evaluation(Base):
+    __tablename__ = "evaluation"
+
+    evaluation_id = Column(Integer, primary_key=True, index=True)
+    marks = Column(Integer,CheckConstraint('marks >= 0 AND marks <= 100')) 
+    pass_fail = Column(String(10))
+    grade = Column(String(5))
+    date_of_evaluation = Column(Date)
+
+    student_id = Column(Integer, ForeignKey("student.student_id"), nullable=False)
+    course_id = Column(Integer, ForeignKey("course.course_id"), nullable=False)
+
